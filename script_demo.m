63a6c5641e03e8a33d4a5a7f23b2ad006a1df661
lmwang <lmwang.nju@gmail.com>
2015-07-21 16:05:35 +0800

    add test.avi

--- script_demo.m
+++ script_demo.m
@@ -3,13 +3,13 @@
 vid_name = 'test.avi';
 
 % idt extraction
-% display('Extract improved trajectories...');
-% system(['./DenseTrackStab -f ',vid_name,' -o ',vid_name(1:end-4),'.bin']);
+display('Extract improved trajectories...');
+system(['./DenseTrackStab -f ',vid_name,' -o ',vid_name(1:end-4),'.bin']);
 
 % TVL1 flow extraction
-% display('Extract TVL1 optical flow field...');
-% mkdir test/
-% system(['./dense_flow -f ',vid_name,' -x test/flow_x -y test/flow_y -b 20 -t 1 -d 3']);
+display('Extract TVL1 optical flow field...');
+mkdir test/
+system(['./dense_flow -f ',vid_name,' -x test/flow_x -y test/flow_y -b 20 -t 1 -d 3']);
 
 % Import improved trajectories
 IDT = import_idt('test.bin',15);
