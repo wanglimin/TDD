85f71af0e47fa74c6b2c9a7f04af07c03cbe541b
lmwang <lmwang.nju@gmail.com>
2015-07-27 12:21:23 +0800

    add extract_fv.m for thumos15 dataset

---
+++ extract_fv.m
@@ -0,0 +1,123 @@
+function  extract_fv(index, power, layer, tag ,dim1, norm)
+
+
+path_tdd = '/nfs/lmwang/thumos15/';
+path1 = [path_tdd,'thumos15_validation_tdd_',tag,'_',layer,'_scale_1_norm_',num2str(norm),'/'];
+path2 = [path_tdd,'thumos15_validation_tdd_',tag,'_',layer,'_scale_2_norm_',num2str(norm),'/'];
+path3 = [path_tdd,'thumos15_validation_tdd_',tag,'_',layer,'_scale_3_norm_',num2str(norm),'/'];
+path4 = [path_tdd,'thumos15_validation_tdd_',tag,'_',layer,'_scale_4_norm_',num2str(norm),'/'];
+path5 = [path_tdd,'thumos15_validation_tdd_',tag,'_',layer,'_scale_5_norm_',num2str(norm),'/'];
+
+path_tdd_fv = '/media/sdd/lmwang/Data/thumos15/';
+path6 = [path_tdd_fv,'thumos15_validation_tdd_',tag,'_',layer,'_scale_1_norm_',num2str(norm),'_fv/'];
+path7 = [path_tdd_fv,'thumos15_validation_tdd_',tag,'_',layer,'_scale_2_norm_',num2str(norm),'_fv/'];
+path8 = [path_tdd_fv,'thumos15_validation_tdd_',tag,'_',layer,'_scale_3_norm_',num2str(norm),'_fv/'];
+path9 = [path_tdd_fv,'thumos15_validation_tdd_',tag,'_',layer,'_scale_4_norm_',num2str(norm),'_fv/'];
+path10 = [path_tdd_fv,'thumos15_validation_tdd_',tag,'_',layer,'_scale_5_norm_',num2str(norm),'_fv/'];
+
+
+num = 256;
+PCA = load(['/home/lmwang/code/tdd/ucf_pca_idt_cnn_vgg_',tag,'_',layer,'_norm_',num2str(norm),'_power_',num2str(power),'.mat']);
+GMM = load(['/home/lmwang/code/tdd/ucf_gmm_',num2str(num),'_pca_',num2str(dim1),'_idt_cnn_vgg_',tag,'_',layer,'_norm_',num2str(norm),'_power_',num2str(power),'.mat']);
+
+folderlist = dir(path3);
+foldername = {folderlist(:).name};
+foldername = setdiff(foldername,{'.','..'});
+
+for i = index
+%     if ~exist([path6,foldername{i}],'dir')
+%         mkdir([path6,foldername{i}]);
+% 	end
+	if ~exist([path7,foldername{i}],'dir')
+        mkdir([path7,foldername{i}]);
+	end
+	if ~exist([path8,foldername{i}],'dir')
+        mkdir([path8,foldername{i}]);
+	end
+	if ~exist([path9,foldername{i}],'dir')
+        mkdir([path9,foldername{i}]);
+	end
+	if ~exist([path10,foldername{i}],'dir')
+        mkdir([path10,foldername{i}]);
+	end
+	
+    filelist = dir([path3,foldername{i},'/*.mat']);
+	tic;
+    for j = 1:length(filelist)
+		
+%       feature = load([path1,foldername{i},'/',filelist(j).name]);
+% 		feature = double(feature.idt_cnn_feature.^power);
+% 		 
+%         if ~isempty(feature)
+%             feature = bsxfun(@minus,feature,PCA.mu);
+%             feature = PCA.U(:,1:dim1)'*feature;
+%             feature = bsxfun(@rdivide,feature,sqrt(PCA.vars(1:dim1)));
+%             coding = vl_fisher(feature,GMM.means,GMM.covariances,GMM.priors);
+% 		else
+% 			
+%             coding = zeros(1,2*num*dim1);
+% 		end
+% 		save([path6,foldername{i},'/',filelist(j).name(1:end-4),'_pca_',num2str(dim1),'_power_',num2str(power),'.mat'],'coding');
+		
+		feature = load([path2,foldername{i},'/',filelist(j).name]);
+		feature = double(feature.idt_cnn_feature.^power);
+		 
+        if ~isempty(feature)
+            feature = bsxfun(@minus,feature,PCA.mu);
+            feature = PCA.U(:,1:dim1)'*feature;
+            feature = bsxfun(@rdivide,feature,sqrt(PCA.vars(1:dim1)));
+            coding = vl_fisher(feature,GMM.means,GMM.covariances,GMM.priors);
+		else
+			
+            coding = zeros(1,2*num*dim1);
+		end
+		save([path7,foldername{i},'/',filelist(j).name(1:end-4),'_pca_',num2str(dim1),'_power_',num2str(power),'.mat'],'coding');
+		
+		feature = load([path3,foldername{i},'/',filelist(j).name]);
+		feature = double(feature.idt_cnn_feature.^power);
+		 
+        if ~isempty(feature)
+            feature = bsxfun(@minus,feature,PCA.mu);
+            feature = PCA.U(:,1:dim1)'*feature;
+            feature = bsxfun(@rdivide,feature,sqrt(PCA.vars(1:dim1)));
+            coding = vl_fisher(feature,GMM.means,GMM.covariances,GMM.priors);
+		else
+			
+            coding = zeros(1,2*num*dim1);
+		end
+		save([path8,foldername{i},'/',filelist(j).name(1:end-4),'_pca_',num2str(dim1),'_power_',num2str(power),'.mat'],'coding');
+		
+		feature = load([path4,foldername{i},'/',filelist(j).name]);
+		feature = double(feature.idt_cnn_feature.^power);
+		 
+        if ~isempty(feature)
+            feature = bsxfun(@minus,feature,PCA.mu);
+            feature = PCA.U(:,1:dim1)'*feature;
+            feature = bsxfun(@rdivide,feature,sqrt(PCA.vars(1:dim1)));
+            coding = vl_fisher(feature,GMM.means,GMM.covariances,GMM.priors);
+		else
+			
+            coding = zeros(1,2*num*dim1);
+		end
+		save([path9,foldername{i},'/',filelist(j).name(1:end-4),'_pca_',num2str(dim1),'_power_',num2str(power),'.mat'],'coding');
+		
+		feature = load([path5,foldername{i},'/',filelist(j).name]);
+		feature = double(feature.idt_cnn_feature.^power);
+		 
+        if ~isempty(feature)
+            feature = bsxfun(@minus,feature,PCA.mu);
+            feature = PCA.U(:,1:dim1)'*feature;
+            feature = bsxfun(@rdivide,feature,sqrt(PCA.vars(1:dim1)));
+            coding = vl_fisher(feature,GMM.means,GMM.covariances,GMM.priors);
+		else
+	
+            coding = zeros(1,2*num*dim1);
+		end
+		save([path10,foldername{i},'/',filelist(j).name(1:end-4),'_pca_',num2str(dim1),'_power_',num2str(power),'.mat'],'coding');
+		
+	end
+	toc;
+end
+
+end
+
--- extract_tdd.m
+++ extract_tdd.m
@@ -61,7 +61,7 @@ if strcmp(tag,'rgb')
     path1 = ['../thumos15_validation_tdd_rgb_',layer,'_scale_',num2str(scale),'_norm_2/'];
     path2 = ['../thumos15_validation_tdd_rgb_',layer,'_scale_',num2str(scale),'_norm_3/'];
     model_def_file = [ 'models/rgb_',layer,'_scale',num2str(scale),'.prototxt'];
-    model_file = 'VGG_CNN_M_2048.caffemodel';
+    model_file = 'spatial.caffemodel';
     sizes_vid = [480,640; 340,454; 240,320; 170,227; 120,160];
     
     folderlist = dir(path_vid);
@@ -79,31 +79,31 @@ if strcmp(tag,'rgb')
         
         filelist = dir([path_vid,foldername{i},'/*.avi']);
         for j = 1:length(filelist)
-
-            if  ~exist([path2,foldername{i},'/',filelist(j).name(1:end-4),'.mat'],'file') 
-			            data = load([path_tra,foldername{i},'/',filelist(j).name(1:end-4),'.mat']);
-            info = data.info;
-            tra = data.tra;
-			if ~isempty(info)
-                tic;
-                filename = [path_vid,foldername{i},'/',filelist(j).name];
-                feature = RGBCNNFeature(filename, 1, sizes_vid(scale,1), sizes_vid(scale,2), model_def_file, model_file, gpu_id);
-                sizes = [8,8; 11.4286,11.4286; 16,16; 22.8571,24;32,34.2587];
-                if max(info(1,:)) > size(feature,4)
-                    max(info(1,:))
-                    size(feature,4)
-                    ind =  info(1,:) <= size(feature,4);
-                    info = info(:,ind);
-                    tra = tra(:,ind);
+            
+            if  ~exist([path2,foldername{i},'/',filelist(j).name(1:end-4),'.mat'],'file')
+                data = load([path_tra,foldername{i},'/',filelist(j).name(1:end-4),'.mat']);
+                info = data.info;
+                tra = data.tra;
+                if ~isempty(info)
+                    tic;
+                    filename = [path_vid,foldername{i},'/',filelist(j).name];
+                    feature = RGBCNNFeature(filename, 1, sizes_vid(scale,1), sizes_vid(scale,2), model_def_file, model_file, gpu_id);
+                    sizes = [8,8; 11.4286,11.4286; 16,16; 22.8571,24;32,34.2587];
+                    if max(info(1,:)) > size(feature,4)
+                        max(info(1,:))
+                        size(feature,4)
+                        ind =  info(1,:) <= size(feature,4);
+                        info = info(:,ind);
+                        tra = tra(:,ind);
+                    end
+                    [cnn_feature1, cnn_feature2] = FeatureMapNormalization(feature);
+                    idt_cnn_feature = TDD(info, tra, cnn_feature1, sizes(scale,1), sizes(scale,2), 1);
+                    save([path1,foldername{i},'/',filelist(j).name(1:end-4),'.mat'],'idt_cnn_feature','-v7.3');
+                    idt_cnn_feature = TDD(info, tra, cnn_feature2, sizes(scale,1), sizes(scale,2), 1);
+                    save([path2,foldername{i},'/',filelist(j).name(1:end-4),'.mat'],'idt_cnn_feature','-v7.3');
+                    toc;
                 end
-                [cnn_feature1, cnn_feature2] = FeatureMapNormalization(feature);
-                idt_cnn_feature = IDTCNN2(info, tra, cnn_feature1, sizes(scale,1), sizes(scale,2), 1);
-                save([path1,foldername{i},'/',filelist(j).name(1:end-4),'.mat'],'idt_cnn_feature','-v7.3');
-                idt_cnn_feature = IDTCNN2(info, tra, cnn_feature2, sizes(scale,1), sizes(scale,2), 1);
-                save([path2,foldername{i},'/',filelist(j).name(1:end-4),'.mat'],'idt_cnn_feature','-v7.3');
-                toc;
             end
-			end
         end
     end
 end
